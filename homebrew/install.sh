e7e6a618036fea8e18b6baa0e0af32c2e3b3a643
concreted <arichuang@gmail.com>
2015-01-17 23:45:27 -0800

    Install commonly used apps with brew/cask

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,7 +12,22 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+brew update
+brew doctor
+
 # Install homebrew packages
 brew install grc coreutils spark
+brew install git
+brew install --HEAD hub
+
+# Install cask
+brew install caskroom/cask/brew-cask
+
+# Install cask packages
+brew cask install alfred
+brew cask install atom
+brew cask install google-chrome 
+brew cask install emacs
+brew cask install flux
 
 exit 0
